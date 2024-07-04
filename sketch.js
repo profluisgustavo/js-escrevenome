0e6c2101e814584acccdff799127653893a6b08e
profluisgustavo <luisgustavoribeiro@prof.educacao.sp.gov.br>
2024-07-04 15:25:09 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let circuloY; // vertical
 function setup() {
   
   createCanvas(400, 400);// width x height
-  background(135, 206, 235);
+  background(3, 120, 230);
   cor = color(random(0,255), random(0,255), random(0,255));
   circuloX = [0,0, 0];
   circuloY = [random(height), random(height), random(height)];
